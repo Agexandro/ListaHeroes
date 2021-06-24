b93bd8f52adc41f10cd667d06215247c14f58d0c
Alejandro G <kleon0313@gmail.com>
2021-06-23 19:40:44 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-
 
 function App() {
   const TOKEN = '228463595783333';
-  const URL = `https://superheroapi.com/api/${TOKEN}/`;
+  const HERO = "batman";
+
+  const URL = `https://www.superheroapi.com/api.php/${TOKEN}/search/${HERO}`;
+
+  //const [hero, SearchHero] = useState("");
+
+  useEffect( async () => {
+    getHero();
+  },[]);
+
+  async function  getHero () {
+    const response = await fetch(URL);
+    const data = response.json();
+    const promise = Promise.resolve(data);
+    let val = null;
+    promise.then((value)=>{
+      val=(value.results);
+
+    for (let i = 0; i < val.length; i++) {
+      let element = val[i];
+      console.log(element.name)
+    }
+    });
+
+  }
 
   return (
     <div className="content">
-      <h1>Heroes</h1>
+      <form className="search-form">
+        <input type="text" className="search-bar"/>
+        <button className="search-button" type="submit">Buscar</button>
+      </form>
     </div>
   );
 
 }
 
 export default App;
+ 
